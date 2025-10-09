d600553b97ada887841d4776aff01d192218c4ac
Naveed Ahmed <na11734@st.habib.edu.pk>
2025-10-09 14:14:13 +0500

    About page revamp

---
+++ hues-web/src/app/DataAbout/history.tsx
@@ -0,0 +1,15 @@
+export const history =  [
+    {
+      quantity: 20,
+      text : "Events",
+      
+    },
+    {
+      quantity: 20,
+      text : "Delegates",
+    },
+    {
+      quantity: 20,
+      text : "Partners",
+    },
+]
---
+++ hues-web/src/app/DataAbout/testimonials.tsx
@@ -0,0 +1,58 @@
+export const testimonials = [
+    {
+        image: <img
+                  src="/Images/profile.png"
+                  alt="Picture of the author"
+                  width = {200}
+                  height = {200}
+
+                />,
+        name: 'Naveed Ahmed',
+        description:'Being part of the Habib University Entrepreneurship Society has been an inspiring experience. HUES empowers students to turn ideas into impact through collaboration, creativity, and innovation. It’s more than a society — it’s a community that encourages leadership, growth, and real-world change.',
+    },
+    {
+        image: <img
+                  src="/Images/profile.png"
+                  alt="Picture of the author"
+                  width = {200}
+                  height = {200}
+                  
+    
+                />,
+        name: 'Naveed Ahmed',
+        description:'Being part of the Habib University Entrepreneurship Society has been an inspiring experience. HUES empowers students to turn ideas into impact through collaboration, creativity, and innovation. It’s more than a society — it’s a community that encourages leadership, growth, and real-world change.',
+    },
+    {
+        image: <img
+                  src="/Images/profile.png"
+                  alt="Picture of the author"
+                  width = {200}
+                  height = {200}
+                  
+    
+                />,
+        name: 'Naveed Ahmed',
+        description:'Being part of the Habib University Entrepreneurship Society has been an inspiring experience. HUES empowers students to turn ideas into impact through collaboration, creativity, and innovation. It’s more than a society — it’s a community that encourages leadership, growth, and real-world change.',
+    },
+    {
+        image: <img
+                  src="/Images/profile.png"
+                  alt="Picture of the author"
+                  width = {200}
+                  height = {200}
+    
+                />,
+        name: 'Naveed Ahmed',
+        description:'Being part of the Habib University Entrepreneurship Society has been an inspiring experience. HUES empowers students to turn ideas into impact through collaboration, creativity, and innovation. It’s more than a society — it’s a community that encourages leadership, growth, and real-world change.',
+    },
+    {
+        image: <img
+                  src="/Images/profile.png"
+                  alt="Picture of the author"
+                  width = {200}
+                  height = {200}
+                />,
+        name: 'Naveed Ahmed',
+        description:'Being part of the Habib University Entrepreneurship Society has been an inspiring experience. HUES empowers students to turn ideas into impact through collaboration, creativity, and innovation. It’s more than a society — it’s a community that encourages leadership, growth, and real-world change.',
+    }
+]
